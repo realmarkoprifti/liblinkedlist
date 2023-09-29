97f6ae06eba4899f860b6a526e621934ecc16fbb
mPenTests <marko.prifti4@gmail.com>
2023-09-29 21:56:54 +0200

    Added more feature blueprints

--- src/linkedlist.hpp
+++ src/linkedlist.hpp
@@ -2,7 +2,7 @@
 #define LINKEDLIST_H
 
 /*
-    Singly-linked list library written in C++ by Marko Prifti (me)
+    Linked list library written in C++ by Marko Prifti (me)
     Will be extended to doubly-linked in the future
     Will add a deleteElement function
 */
@@ -19,6 +19,7 @@ ListNode *createLinkedList(int *numArr, int size);
 void insertElement(ListNode *head, int val);
 void deleteElement(ListNode *head, int val);
 int listLength(ListNode *head);
+ListNode *sortList(ListNode *head);
 void printLinkedList(ListNode *head);
 
 #endif
