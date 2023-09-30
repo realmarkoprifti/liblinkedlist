61135b8f31543661b744c81fea0f79fb29f52d14
mPenTests <marko.prifti4@gmail.com>
2023-09-30 16:25:36 +0200

    Added implementation for double and singly linked list

--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -4,6 +4,11 @@
 
 ListNode *createLinkedList(int *numArr, int size)
 {
+    if (size <= 1)
+    {
+        return NULL;
+    }
+
     ListNode *head = new ListNode;
     head = NULL;
 
@@ -13,9 +18,75 @@ ListNode *createLinkedList(int *numArr, int size)
         newNode->val = numArr[i];
         newNode->prev = NULL;
         newNode->next = head;
+        newNode->isHead = false;
+        newNode->isTail = false;
+
+        if (i == 0)
+        {
+            newNode->isTail = true;
+        }
+
+        else if (i == size - 1)
+        {
+            newNode->isHead = true;
+        }
 
         head = newNode;
     }
+
+    return head;
+}
+
+
+ListNode *createDoublyLinkedList(int *numArr, int size)
+{
+    if (size <= 1)
+    {
+        return NULL;
+    }
+
+    ListNode *head = new ListNode;
+    head = NULL;
+
+    for (int i = 0; i < size; i++)
+    {
+        ListNode *newNode = new ListNode;
+        newNode->val = numArr[i];
+        newNode->next = head;
+        newNode->isHead = false;
+        newNode->isTail = false;
+
+        if (i == 0)
+        {
+            newNode->isTail = true;
+        }
+
+        else if (i == size - 1)
+        {
+            newNode->isHead = true;
+        }
+
+        head = newNode;
+    }
+
+    ListNode *ptr = new ListNode;
+    ListNode *prev = new ListNode;
+    prev = NULL;
+
+    for (ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        ptr->prev = prev;
+
+        if (ptr->isHead)
+        {
+            ptr->prev = NULL;
+        }
+
+        prev = ptr;
+    }
+
+    delete ptr;
+    delete prev;
 
     return head;
 }
